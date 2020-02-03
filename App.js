131e756a4cffceb2e9d5e8fc3d62d1b7fc89b217
Buddy Agyin <agyin3@gmail.com>
2020-02-02 22:15:26 -0700

    add styling to input and todos

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, TextInput, TouchableHighlight, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View, TextInput, TouchableOpacity, Dimensions, Switch } from 'react-native';
 
+const {height, width} = Dimensions.get('window')
 export default function App() {
   const [todos, setTodos] = useState(['React Native Tutorial'])
   const [newTodo, setNewTodo] = useState()
@@ -16,14 +17,17 @@ export default function App() {
   }
   return (
     <View style={styles.container}>
-      <Text>Mobile To-Do List</Text>
-      <TextInput style={styles.input} onChangeText={handleChange} value={newTodo} />
-      <TouchableOpacity onPress={handlePress}>
-        <Text>Submit</Text>
-      </TouchableOpacity>
-      <View>
+      <View style={styles.inputContainer}>
+        <TextInput style={styles.input} onChangeText={handleChange} value={newTodo} />
+        <TouchableOpacity style={styles.button} onPress={handlePress}>
+          <Text style={styles.buttonText}>Submit</Text>
+        </TouchableOpacity>
+      </View>
+      <View style={styles.todos}>
         {todos.map((todo, index) => (
-          <Text key={index}>{todo}</Text>
+          <View key={index} style={styles.textContainer}>
+            <Text style={styles.todoText}>{todo}</Text>
+          </View>
         ))}
       </View>
     </View>
@@ -35,13 +39,48 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: 100
+  },
+  inputContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    paddingLeft:  20,
+    paddingRight: 20,
+    marginBottom: 40
   },
   input: {
+    flex: 0.7,
     height: 50,
-    width: 150,
     borderRadius: 4,
-    borderWidth: 1,
-    borderColor: 'aqua'
+    borderBottomWidth: 2,
+    borderBottomColor: 'aqua',
+    marginRight: 10,
+    fontSize: 24,
+  }, 
+  button: {
+    flex: 0.3,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#ef2345',
+    height: 50,
+    borderRadius: 10
+  },
+  buttonText: {
+    color: '#ffffff',
+    fontSize: 19
+  },
+  todos: {
+    alignItems: 'center',
+  },
+  textContainer: {
+    borderBottomWidth: 3,
+    borderBottomColor: 'green',
+    borderRadius: 5,
+    marginBottom: 15,
+    width: (width / 10) * 9,
+  },
+  todoText: {
+    fontSize: 18,
   }
 });
