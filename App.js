4baf649cfcc3f4da211f49937cfbb65e9733b3a9
Buddy Agyin <agyin3@gmail.com>
2020-02-19 22:25:26 -0700

    refractor to root element, add provider and store

--- App.js
+++ App.js
@@ -1,101 +1,22 @@
-import React, { useState, useEffect } from 'react';
-import { StyleSheet, Text, View, TextInput, TouchableOpacity, Dimensions } from 'react-native';
-import axios from 'axios'
+import React from 'react'
+import Todo from './Todo.js'
+import { Provider } from 'react-redux'
+import { store } from './store.js'
+import { StyleSheet } from 'react-native'
 
-const {height, width} = Dimensions.get('window')
-export default function App() {
-  const [todos, setTodos] = useState([])
-  const [newTodo, setNewTodo] = useState()
-
-  const fetchData = () => {
-    axios.get('http://localhost:3000/todos')
-    .then(res => setTodos(res.data))
-    .catch(err => console.log(err))
-  }
-
-  useEffect(() => {
-    fetchData()
-  }, [])
-
-  const handleChange = text => {
-    setNewTodo(text)
-  }
-
-  const handlePress = e => {
-    axios.post('http://localhost:3000/todos', {
-      name: newTodo
-    })
-    .then(res => console.log(res))
-    .catch(err => console.log(err))
-    fetchData()
-    setNewTodo('')
-  }
-  return (
-    <View style={styles.container}>
-      <View style={styles.inputContainer}>
-        <TextInput style={styles.input} onChangeText={handleChange} value={newTodo} />
-        <TouchableOpacity style={styles.button} onPress={handlePress}>
-          <Text style={styles.buttonText}>Submit</Text>
-        </TouchableOpacity>
-      </View>
-      <View style={styles.todos}>
-        {todos.map((todo, index) => (
-          <View key={index} style={styles.textContainer}>
-            <Text style={styles.todoText}>{todo.name}</Text>
-          </View>
-        ))}
-      </View>
-    </View>
-  );
+export default App = () => {
+    return(
+    <Provider store={store}>
+        <Todo />
+    </Provider>
+)
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    paddingTop: 100
-  },
-  inputContainer: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    paddingLeft:  20,
-    paddingRight: 20,
-    marginBottom: 40
-  },
-  input: {
-    flex: 0.7,
-    height: 50,
-    borderRadius: 4,
-    borderBottomWidth: 2,
-    borderBottomColor: 'aqua',
-    marginRight: 10,
-    fontSize: 24,
-  }, 
-  button: {
-    flex: 0.3,
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: '#ef2345',
-    height: 50,
-    borderRadius: 10
-  },
-  buttonText: {
-    color: '#ffffff',
-    fontSize: 19
-  },
-  todos: {
-    alignItems: 'center',
-  },
-  textContainer: {
-    borderBottomWidth: 3,
-    borderBottomColor: 'green',
-    borderRadius: 5,
-    marginBottom: 15,
-    width: (width / 10) * 9,
-  },
-  todoText: {
-    fontSize: 18,
-  }
-});
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        paddingTop: 100
+      },
+})
